50ca99497893369311716e60c4ce1caa84fb26c7
Himanshu Khudania <hkhudania@gmail.com>
2020-04-24 09:39:56 +0530

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const demoPage = () => (
+    <Layout>
+        <SEO title="Demo" />
+        <h1>This is Contact Form Page</h1>
+        <p>how are you?</p>
+        <Link to="/">Go back to the homepage</Link>
+    </Layout>
+)
+
+export default demoPage
